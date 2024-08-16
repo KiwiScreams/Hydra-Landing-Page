50fd23f2c3dde1d0958dca3fb8c627885aed548c
KiwiScreams <screamskiwi3@gmail.com>
2024-08-16 20:39:27 +0400

    tech panel

---
+++ src/components/tech/Tech.jsx
@@ -0,0 +1,20 @@
+import "./Tech.css";
+const Technology = () => {
+  return (
+    <>
+      <section className="tech-section">
+        <div className="tech-container">
+          <div className="text-container">
+            <h1>TECHNOLOGIES & HARDWARE</h1>
+            <h1>USED BY HYDRA VR.</h1>
+          </div>
+          <div className="arrow">
+            <i class="fa-solid fa-chevron-down"></i>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Technology;
