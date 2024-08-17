7dc96c37737985be14ba17ac31bea43c783c5086
KiwiScreams <screamskiwi3@gmail.com>
2024-08-17 20:56:28 +0400

    services component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Contacts from "../../components/contact/Contacts";
 import Layout from "../../components/layout/Layout";
+import Services from "../../components/services/Services";
 import Technology from "../../components/tech/Tech";
 
 const Home = () => {
   return (
     <>
       <Layout />
+      <Services />
       <Technology />
       <Contacts />
     </>
